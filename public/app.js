f3b55c979f0b2d92c81abcf70652cb8c3a924d88
Matthew Hodges <52683582+matthewhodges56@users.noreply.github.com>
2025-03-04 14:19:54 -0600

    fix

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@ document.head.appendChild(favicon);
 
 async function getGeminiSummary(weatherData) {
     try {
-        const response = await fetch("http://localhost:3000/getWeatherSummary", {
+        const response = await fetch("https://wstat.onrender.com/getWeatherSummary", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ weatherData })
@@ -132,7 +132,7 @@ function showWeather() {
 }
 
 function getWeather(city) {
-    fetch("http://localhost:3000/api/weather?city=" + city)
+    fetch("https://wstat.onrender.com/api/weather?city=" + city)
     .then(response => response.json())
     .then(data => {
         if (data.error) {
